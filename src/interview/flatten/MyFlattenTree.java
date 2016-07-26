6b4d21e04132bc90627d32d5083a755eeb1e73d8
TAPAN PAUL <tapan.paul@gmail.com>
2016-07-26 23:05:42 +1000

    ass3 : initial version

---
+++ src/interview/flatten/MyFlattenTree.java
@@ -0,0 +1,14 @@
+package interview.flatten;
+
+import java.util.List;
+
+/**
+ * @tapan .
+ */
+public class MyFlattenTree<T> implements FlattenTree<T> {
+
+    @Override
+    public List<T> flattenInOrder(Tree<T> tree) {
+        return null;
+    }
+}
